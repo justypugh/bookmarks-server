c19c2eed142305c00456ed5877c69064e9604d8a
Justin Pugh <justypugh@icloud.com>
2020-05-06 08:52:59 -0700

    second commit

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'fake-api-token',
+    DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
 }
